0b1ff509d9ecf045a1db12baa772a4cb4720589b
Michael Mc Donnell <michael@mcdonnell.dk>
2024-12-02 22:23:14 -0500

    Don't compile Apple only blurmac
    Don't compile Apple only blurmac package on Linux and Windows. It was
    causing an error in VS Code with rust-analyzer.
    
    Signed-off-by: Michael Mc Donnell <michael@mcdonnell.dk>

--- third_party/blurmac/src/lib.rs
+++ third_party/blurmac/src/lib.rs
@@ -5,24 +5,28 @@
 // This file may not be copied, modified, or distributed except
 // according to those terms.
 
-#[macro_use]
-extern crate log;
-#[macro_use]
-extern crate objc;
+cfg_if::cfg_if! {
+    if #[cfg(target_os = "macos")] {
+        #[macro_use]
+        extern crate log;
+        #[macro_use]
+        extern crate objc;
 
-mod adapter;
-mod delegate;
-mod device;
-mod discovery_session;
-mod framework;
-mod gatt_characteristic;
-mod gatt_descriptor;
-mod gatt_service;
-mod utils;
+        mod adapter;
+        mod delegate;
+        mod device;
+        mod discovery_session;
+        mod framework;
+        mod gatt_characteristic;
+        mod gatt_descriptor;
+        mod gatt_service;
+        mod utils;
 
-pub use adapter::BluetoothAdapter;
-pub use device::BluetoothDevice;
-pub use discovery_session::BluetoothDiscoverySession;
-pub use gatt_characteristic::BluetoothGATTCharacteristic;
-pub use gatt_descriptor::BluetoothGATTDescriptor;
-pub use gatt_service::BluetoothGATTService;
+        pub use adapter::BluetoothAdapter;
+        pub use device::BluetoothDevice;
+        pub use discovery_session::BluetoothDiscoverySession;
+        pub use gatt_characteristic::BluetoothGATTCharacteristic;
+        pub use gatt_descriptor::BluetoothGATTDescriptor;
+        pub use gatt_service::BluetoothGATTService;
+    }
+}
